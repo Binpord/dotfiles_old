958c034af9d4f273ba0ff2a2ad70e78819db6076
Shiyanov Vadim <vadimsh853@gmail.com>
2018-11-13 23:05:50 +0300

    Add local sbin to PATH

--- .zsh/general/environment.zsh
+++ .zsh/general/environment.zsh
@@ -1,6 +1,6 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export PATH=$HOME/miniconda3/bin:$HOME/bin:$PATH
+export PATH=/usr/local/sbin:$HOME/miniconda3/bin:$HOME/bin:$PATH
 export ZSH=$HOME/.oh-my-zsh
 export EDITOR='vim'
 export KEYTIMEOUT=1
